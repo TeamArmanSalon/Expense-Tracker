1398d3a5b7bd24842f9e33f7b79836fb11517805
shin--idk <joshanlucmayan838@gmail.com>
2025-11-10 12:42:57 +0800

    condition for entering name and strong password

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -30,10 +30,41 @@ public class ExpenseTracker {
     }
 
     private static void registerUser(){
+
+        String name;
+        String password = "";
+
         System.out.print("Enter Name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter Password: ");
-        String password = scanner.nextLine();
+        name = scanner.nextLine();
+
+        while(true){
+            if(!name.matches("[a-z-A-Z ,.]+")){
+                System.out.println("[ We only accept Name that contains letters, space, comma, and dot! ]");
+                System.out.print("Enter Name: ");
+                name = scanner.nextLine();
+            }
+            else{
+                break;
+            }
+        }
+
+        String regex = "^(?=.{12,64}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).*$";
+
+        while(true){
+            System.out.print("Enter Password: ");
+            password = scanner.nextLine();
+            if(password.isEmpty()){
+                System.out.println("[ Must not be empty, Please try again! ]");
+            }
+            else if(!password.matches(regex)){
+                System.out.println("[ Password must be minimum 12 chars, at least one lowercase," +
+                        " one uppercase, one digit, and one symbol. Please Try again! ]");
+            }
+            else{
+                break;
+            }
+        }
+
 
         user = new User(name, password);
         user.setAccount(new Account(0.0, name));
